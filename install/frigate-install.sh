6ce7aacc2ba7c3f8f94ca28a8267f5fc1ca6049d
Wim <11030068+wimb0@users.noreply.github.com>
2025-02-18 11:35:56 +0100

    Update frigate-install.sh

--- install/frigate-install.sh
+++ install/frigate-install.sh
@@ -43,12 +43,9 @@ msg_info "Installing sqlite_vec"
 SQLITE_VEC_VERSION="0.1.3"
 source /etc/os-release
 
-if [[ "$VERSION_ID" == "12" ]]; then
-    sed -i '/^Types:/s/deb/& deb-src/' /etc/apt/sources.list.d/debian.sources
-else
-    cp /etc/apt/sources.list /etc/apt/sources.list.d/sources-src.list
-    sed -i 's|deb http|deb-src http|g' /etc/apt/sources.list.d/sources-src.list
-fi
+cp /etc/apt/sources.list /etc/apt/sources.list.d/sources-src.list
+sed -i 's|deb http|deb-src http|g' /etc/apt/sources.list.d/sources-src.list
+
 $STD apt-get update
 $STD apt-get install -y {build-dep,sqlite3,gettext,git}
 mkdir /tmp/sqlite_vec
